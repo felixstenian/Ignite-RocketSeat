b1968596a89745cb7d3c1c1cd83ba7296261d3cc
felixstenian <felixstenian@gmail.com>
2022-09-30 10:18:30 -0300

    Component: Header

--- Projeto-01_fundamentos-reactjs/src/components/Header.tsx
+++ Projeto-01_fundamentos-reactjs/src/components/Header.tsx
@@ -1,8 +1,10 @@
 import style from './Header.module.css'
+import igniteLogo from '../assets/Ignite-logo.svg'
 
 export const Header = () => {
   return (
     <header className={style.header}>
+      <img src={igniteLogo} alt='Logo do Ignite' />
       <strong>Ignite Feed</strong>
     </header>
   )
