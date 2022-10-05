efc7086ebee3c766b07b8e01147f8a3a1b46b9c4
felixstenian <felixstenian@gmail.com>
2022-10-04 21:10:46 -0300

    Componente: Siderbar - Finalizando componente

--- Projeto-01_fundamentos-reactjs/src/components/Sidebar.tsx
+++ Projeto-01_fundamentos-reactjs/src/components/Sidebar.tsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import style from './Sidebar.module.css'
 
 export const Sidebar = () => {
@@ -8,12 +10,16 @@ export const Sidebar = () => {
         src='https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=380&q=50' 
       />
       <div className={style.profile}>
+        <img className={style.avatar} src='https://avatars.githubusercontent.com/u/35822492?v=4' alt='Felix foto' />
         <strong>Felix</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href='#'>Editar seu perfil</a>
+        <a href='#'>
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   )
